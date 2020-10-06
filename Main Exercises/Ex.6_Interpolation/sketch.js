6af9985fc4da58d435bcfbd541fc8db78db4d388
John Carlo Mendoza Ramos <johncarlomramos97@gmail.com>
2020-10-06 19:02:33 +0100

    Update on Ex.6:
    - Simplified array loop for rectangles. Removed class.
    - Added color() functions to prepare for interpolation.

--- Main Exercises/Ex.6_Interpolation/sketch.js
+++ Main Exercises/Ex.6_Interpolation/sketch.js
@@ -1,36 +1,20 @@
-let rects = []; 
-
 function setup() {
-    createCanvas(windowWidth, windowHeight);
-    // background(127);
+    createCanvas(700, 700);
 
-    for (let i = 0; i < 10; i++) {
-        rects.push(new Rectangle());
-    }
+    colorMode(HSB, 360, 100, 100); // Max. HSB
+    noStroke();
+    smooth();
 }
 
 function draw() {
-    background(127);
+    background(0);
 
-    for (let i; i < rects.length; i++) {
-        rects[i].display();
-    }
-}
+    let fromColour = color(60, 100, 100);
+    let toColour = color(280, 100, 100);
 
-class Rectangle {
-    constructor() {
-        this.x = random(width);
-        this.y = random(height);
-        this.from = color(0, 255, 0);
-        this.to = color(0, 0, 255);
-        this.interA = lerpColor(this.from, this.to, 0.33);
-        this.interB = lerpColor(this.from, this.to, 0.66);
-    }
-
-    display() {
-        stroke(this.interB);
-        strokeWeight(1);
-        fill(this.interA);
-        rect(this.x, this.y, 50, 50);
+    for (let i = 0; i < 10; i++) {
+        fill(fromColour);
+        stroke(toColour);
+        rect((i * 60) + 30, 100, 50, 50);
     }
 }
