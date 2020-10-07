754e7f1a5cf4334058fc867925f5905664ee11ac
John Carlo Mendoza Ramos <johncarlomramos97@gmail.com>
2020-10-07 18:31:18 +0100

    Update on dynamic objects:
    - Further experimentation with objects.
    - Created abstract spirals.
    - Interactions temporarily consists of changing the interpolation values
    by moving the mouse cursor = both x and y axis.

--- Main Exercises/Ex.6_Interpolation/sketch.js
+++ Main Exercises/Ex.6_Interpolation/sketch.js
@@ -1,3 +1,8 @@
+// Time:
+let t = 0; 
+
+let move = 0;
+
 function setup() {
     createCanvas(windowWidth, windowHeight);
 
@@ -7,7 +12,8 @@ function setup() {
 }
 
 function draw() {
-    background(0, 0, 10);
+    // background(0, 0, 5);
+    background(0, 0, 0);
 
     spiralExpand();
     spiralAbstract();
@@ -15,73 +21,98 @@ function draw() {
 }
 
 function spiralExpand() {
+    let changeX = map(mouseX, 0, width, 17, 360);
+    let changeY = map(mouseY, 0, width, 10, 235);
+    
     // let fromColour = color(60, 100, 100);
-    let toColour = color(280, 100, 100);
-    let fromColour = color(mouseX, 100, 100);
-    // let toColour = color(mouseY, 100, 100);
+    // let toColour = color(280, 100, 100);
+    let fromColour = color(changeX, 100, 100);
+    let toColour = color(changeY, 100, 100);
 
-    let r1 = 0;
-    let r2 = 0;
-    // let step = 1;
-    let step = mouseX / 200;
-    let spiralWidth = 20.0;
-    let dw = spiralWidth / 250;
+    let spiralOne = 0;
+    let spiralTwo = 0;
+    let step = 1;
+    // let step = mouseX / 200;
+    // let spiralWidth = 20;
+    let spiralWidth = 1;
+    let dynamicWidth = spiralWidth / 250;
     
     beginShape(TRIANGLE_STRIP);
-        translate(width/6, height/2);
+        translate(width/4, height/2);
 
         for (let i = 0; i <= 360 ; i++) {
-            r1 += step;
-            spiralWidth -= dw;
-            r2 = r1 + spiralWidth;
+            spiralOne += step;
+            // spiralWidth -= dynamicWidth;
+            spiralWidth += dynamicWidth;
+            spiralTwo = spiralOne + spiralWidth;
             
-            let ang = PI / 30;
+            let ang = PI / 20;
             // let ang = mouseX / 1999;
-            let r1x = r1 * sin(ang * i);
-            let r1y = r1 * cos(ang * i);
-            let r2x = r2 * sin(ang * i);
-            let r2y = r2 * cos(ang * i);
+
+            let spiralOneX = spiralOne * sin(ang * i * t);
+            let spiralOneY = spiralOne * cos(ang * i * t);
+
+            let spiralTwoX = spiralTwo * sin(ang * i * t);
+            let spiralTwoY = spiralTwo * cos(ang * i * t);
             
             fill(lerpColor(fromColour, toColour, i / 360)); 
-            stroke(lerpColor(fromColour, toColour, i / 360));
-            vertex(r1x, r1y);
-            vertex(r2x, r2y);
+            stroke(0, 0, 0);
+            strokeWeight(0.75);
+            // stroke(lerpColor(fromColour, toColour, i / 360));
+            vertex(spiralOneX, spiralOneY);
+            vertex(spiralTwoX, spiralTwoY);
         }
     endShape();
   }
 
   function spiralAbstract() {
-    let fromColour = color(60, 100, 100);
-    // let toColour = color(280, 100, 100);
-    // let fromColour = color(mouseX, 100, 100);
-    let toColour = color(mouseY, 100, 100);
+    let changeX = map(mouseX, 0, width, 120, 60);
+    let changeY = map(mouseY, 0, width, 300, 150);
 
-    let r1 = 0;
-    let r2 = 0;
+    // let fromColour = color(60, 100, 100);
+    // let toColour = color(280, 100, 100);
+    let fromColour = color(changeX, 100, 100);
+    let toColour = color(changeY, 100, 100);
+
+    let spiralOne = 0;
+    let spiralTwo = 0;
     let step = 1;
     // let step = mouseX / 200;
     let spiralWidth = 20.0;
-    let dw = spiralWidth / 250;
+    let dynamicWidth = spiralWidth / 250;
     
     beginShape(TRIANGLE_STRIP);
         translate(width/2, 0);
 
-        for (let i = 0; i <= 240 ; i++) {
-            r1 += step;
-            spiralWidth -= dw;
-            r2 = r1 + spiralWidth;
+        for (let i = 0; i <= 50 ; i++) {
+            spiralOne += step;
+            spiralWidth -= dynamicWidth;
+            spiralTwo = spiralOne + spiralWidth;
             
-            // let ang = PI / 30;
-            let ang = mouseX / 1999;
-            let r1x = r1 * sin(ang * i);
-            let r1y = r1 * cos(ang * i);
-            let r2x = r2 * sin(ang * i);
-            let r2y = r2 * cos(ang * i);
+            let ang = PI / -2;
+            // let ang = mouseX / 999;
+            // let ang = mouseX / -999;
+
+            let spiralOneX = spiralOne * sin(ang * i * t);
+            let spiralOneY = spiralOne * cos(ang * i * t);
+
+            let spiralTwoX = spiralTwo * sin(ang * i * t);
+            let spiralTwoY = spiralTwo * cos(ang * i * t);
             
-            fill(lerpColor(fromColour, toColour, i / 240)); 
-            stroke(lerpColor(fromColour, toColour, i / 240));
-            vertex(r1x, r1y);
-            vertex(r2x, r2y);
+            fill(lerpColor(fromColour, toColour, i / 50)); 
+            // stroke(0, 0, 100);
+            // strokeWeight(0.5);
+            // stroke(lerpColor(fromColour, toColour, i / 50));
+            vertex(spiralOneX, spiralOneY);
+            vertex(spiralTwoX, spiralTwoY);
         }
-    endShape();
-  }
+        endShape();
+        t = t + 0.005;
+    }
+
+    // function mouseDragged() {
+    //     move += move;
+    //     if (move > width + height) {
+    //       move = 0;
+    //     }
+    //   }
