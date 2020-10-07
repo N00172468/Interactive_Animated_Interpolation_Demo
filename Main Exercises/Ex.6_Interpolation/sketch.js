1214ba58a6045429f788656df5869784fa9f485a
John Carlo Mendoza Ramos <johncarlomramos97@gmail.com>
2020-10-07 09:54:45 +0100

    Ex.6 Experimentation:
    - Experimenting with various objects.

--- Main Exercises/Ex.6_Interpolation/sketch.js
+++ Main Exercises/Ex.6_Interpolation/sketch.js
@@ -1,5 +1,5 @@
 function setup() {
-    createCanvas(700, 700);
+    createCanvas(windowWidth, windowHeight);
 
     colorMode(HSB, 360, 100, 100); // Max. HSB
     noStroke();
@@ -7,13 +7,30 @@ function setup() {
 }
 
 function draw() {
-    background(0, 0, 10);
+    background(0, 0, 10, 70);
 
     let fromColour = color(60, 100, 100);
     let toColour = color(280, 100, 100);
 
     for (let i = 0; i < 10; i++) {
-        fill(lerpColor(fromColour, toColour, i / 10));
-        rect((i * 60) + 30, 100, 50, 50);
+        push();
+            fill(lerpColor(fromColour, toColour, i / 10));
+            rect((i * 60) + 30, (i * 60) + 30, 50, 50);
+        pop();
+
+        push();
+            noFill();
+            stroke(lerpColor(fromColour, toColour, i / 10));
+            strokeWeight(5);
+            // rect((i * 60) + 30, (i * 60) + 30, 50, 50);
+            ellipse((i * mouseX) + mouseX, (i * mouseY) + mouseY, i * 10, i * 10);
+        pop();
+
+        push();
+            noFill();
+            stroke(lerpColor(fromColour, toColour, i / 10));
+            strokeWeight(5);
+            ellipse(1920/2, 937/2, i * 25, i * 25);
+        pop();
     }
 }
