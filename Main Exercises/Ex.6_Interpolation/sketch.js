32f9a81c8b752dd7f87ce981ec1ba1582788803e
John Carlo Mendoza Ramos <johncarlomramos97@gmail.com>
2020-10-07 16:38:31 +0100

    Update on Ex.6 - Interpolation:
    - Added another spiral object.
    - Spiral A = spiralExpand
    - Spiral B - spiralAbstract
    - Interaction mouse cursor x-axis only. Will update interaction.

--- Main Exercises/Ex.6_Interpolation/sketch.js
+++ Main Exercises/Ex.6_Interpolation/sketch.js
@@ -1,5 +1,3 @@
-// let t = 0; // time variable
-
 function setup() {
     createCanvas(windowWidth, windowHeight);
 
@@ -9,82 +7,36 @@ function setup() {
 }
 
 function draw() {
-    // background(0, 0, 10, 0.40);
     background(0, 0, 10);
 
-    let fromColour = color(60, 100, 100);
-    let toColour = color(280, 100, 100);
-
-    for (let i = 0; i < 10; i++) {
-        // push();
-        //     noStroke();
-        //     fill(lerpColor(fromColour, toColour, i / 10));
-        //     rect((i * 60) + 30, (i * 60) + 30, 50, 50);
-        // pop();
-
-        // push();
-        //     noFill();
-        //     stroke(lerpColor(fromColour, toColour, i / 10));
-        //     strokeWeight(5);
-        //     ellipse((i * mouseX) + mouseX, (i * mouseY) + mouseY, i * 10, i * 10);
-        // pop();
-
-        push();
-            translate(width/10,height/5);
-            noFill();
-            stroke(lerpColor(fromColour, toColour, i / 10));
-            strokeWeight(5);
-            ellipse(0, 0, i * 25, i * 25);
-        pop();
-    }
-
-    // make a x and y grid of ellipses
-    // for (let x = 0; x <= width; x = x + 30) {
-    //     let fromColour = color(60, 100, 100);
-    //     let toColour = color(280, 100, 100);
-
-    //     for (let y = 0; y <= height; y = y + 30) {
-    //     // starting point of each circle depends on mouse position
-    //     const xAngle = map(mouseX, 0, width, -4 * PI, 4 * PI, true);
-    //     const yAngle = map(mouseY, 0, height, -4 * PI, 4 * PI, true);
-    //     // and also varies based on the particle's location
-    //     const angle = xAngle * (x / width) + yAngle * (y / height);
-
-    //     // each particle moves in a circle
-    //     const myX = x + 20 * cos(2 * PI * t + angle);
-    //     const myY = y + 20 * sin(2 * PI * t + angle);
-
-    //     noStroke();
-    //     fill(lerpColor(fromColour, toColour, x / mouseX)); 
-    //     ellipse(myX, myY, 10); // draw particle
-    //     }
-    // }
-
-    // t = t + 0.01; // update time
-
-    spiral();
+    spiralExpand();
+    spiralAbstract();
     
 }
 
-function spiral() {
-    let fromColour = color(60, 100, 100);
+function spiralExpand() {
+    // let fromColour = color(60, 100, 100);
     let toColour = color(280, 100, 100);
+    let fromColour = color(mouseX, 100, 100);
+    // let toColour = color(mouseY, 100, 100);
 
     let r1 = 0;
     let r2 = 0;
-    let step = 1;
+    // let step = 1;
+    let step = mouseX / 200;
     let spiralWidth = 20.0;
     let dw = spiralWidth / 250;
     
     beginShape(TRIANGLE_STRIP);
-        translate(width/2, height/2);
+        translate(width/6, height/2);
 
         for (let i = 0; i <= 360 ; i++) {
             r1 += step;
             spiralWidth -= dw;
             r2 = r1 + spiralWidth;
             
-            let ang = mouseX / 666;
+            let ang = PI / 30;
+            // let ang = mouseX / 1999;
             let r1x = r1 * sin(ang * i);
             let r1y = r1 * cos(ang * i);
             let r2x = r2 * sin(ang * i);
@@ -92,6 +44,42 @@ function spiral() {
             
             fill(lerpColor(fromColour, toColour, i / 360)); 
             stroke(lerpColor(fromColour, toColour, i / 360));
+            vertex(r1x, r1y);
+            vertex(r2x, r2y);
+        }
+    endShape();
+  }
+
+  function spiralAbstract() {
+    let fromColour = color(60, 100, 100);
+    // let toColour = color(280, 100, 100);
+    // let fromColour = color(mouseX, 100, 100);
+    let toColour = color(mouseY, 100, 100);
+
+    let r1 = 0;
+    let r2 = 0;
+    let step = 1;
+    // let step = mouseX / 200;
+    let spiralWidth = 20.0;
+    let dw = spiralWidth / 250;
+    
+    beginShape(TRIANGLE_STRIP);
+        translate(width/2, 0);
+
+        for (let i = 0; i <= 240 ; i++) {
+            r1 += step;
+            spiralWidth -= dw;
+            r2 = r1 + spiralWidth;
+            
+            // let ang = PI / 30;
+            let ang = mouseX / 1999;
+            let r1x = r1 * sin(ang * i);
+            let r1y = r1 * cos(ang * i);
+            let r2x = r2 * sin(ang * i);
+            let r2y = r2 * cos(ang * i);
+            
+            fill(lerpColor(fromColour, toColour, i / 240)); 
+            stroke(lerpColor(fromColour, toColour, i / 240));
             vertex(r1x, r1y);
             vertex(r2x, r2y);
         }
